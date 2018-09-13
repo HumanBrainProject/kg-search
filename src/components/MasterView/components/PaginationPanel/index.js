5029552338b4a6c90e834d5d22d7f7873ed5ea02
gdenerva <gilles.denervaud@epfl.ch>
2018-09-13 10:55:41 +0200

    mobile paging optimizations

--- src/components/MasterView/components/PaginationPanel/index.js
+++ src/components/MasterView/components/PaginationPanel/index.js
@@ -48,7 +48,7 @@ export class PaginationPanel extends PureComponent {
   }
   get pageScope() {
     const width = windowWidth();
-    return (width >= 1800)?3:(width >= 1600)?2:1;
+    return (width >= 1800)?3:(width >= 1600)?2:(width >= 1200)?1:0;
   }
   updatePageScope() {
     this.setState({pageScope: this.pageScope});
