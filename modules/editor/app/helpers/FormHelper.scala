5772e8d2339525b72bbbd4afcbd91f4b0abb8c28
David Kunzmann <david.kunzmann@epfl.ch>
2018-09-13 10:53:37 +0200

    Fix Json Error upon save

--- modules/editor/app/helpers/FormHelper.scala
+++ modules/editor/app/helpers/FormHelper.scala
@@ -157,34 +157,18 @@ object FormHelper {
 
   def removeKey(jsValue: JsValue):JsValue = {
     if(jsValue.validateOpt[JsObject].isSuccess){
-      removeKeyOfObj(jsValue.as[JsObject]).as[JsValue]
+      val correctedObj = jsValue.as[JsObject] - "description" - "label" - "status" - "childrenStatus"
+      val res = correctedObj.value.map{
+        case (k ,v) =>
+          k -> removeKey(v)
+      }
+      Json.toJson(res)
     }else if(jsValue.validateOpt[JsArray].isSuccess){
-      removeKeyOfArr(jsValue.as[JsArray]).as[JsValue]
+      Json.toJson(jsValue.as[JsArray].value.map(removeKey))
     }else{
       jsValue
     }
 
-  }
-
-  def removeKeyOfArr(jsArray: JsArray):JsArray = {
-    Json.toJson(jsArray.value.map(removeKey)).as[JsArray]
-  }
-
-  def removeKeyOfObj(jsObject: JsObject): JsObject = {
-    val correctedObj = jsObject - "description" - "label"
-    val res = correctedObj.value.map{
-      case (k ,v) =>
-        if(v.validateOpt[JsArray].isSuccess){
-          val arr = v.as[JsArray]
-          k -> removeKeyOfArr(arr)
-        }else if (v.validateOpt[JsObject].isSuccess){
-          k -> removeKeyOfObj(v.as[JsObject])
-        }else{
-          k -> removeKey(v)
-        }
-
-    }
-    Json.toJson(res).as[JsObject]
   }
 
 }
