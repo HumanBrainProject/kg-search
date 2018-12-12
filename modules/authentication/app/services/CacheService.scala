800b814b42e8c400d580029504c9c676634ae30a
David Kunzmann <davidkunzmann@gmail.com>
2018-12-12 08:31:40 +0100

    Added admin route to flush cache

--- modules/authentication/app/services/CacheService.scala
+++ modules/authentication/app/services/CacheService.scala
@@ -1,21 +1,22 @@
 /*
-*   Copyright (c) 2018, EPFL/Human Brain Project PCO
-*
-*   Licensed under the Apache License, Version 2.0 (the "License");
-*   you may not use this file except in compliance with the License.
-*   You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-*   Unless required by applicable law or agreed to in writing, software
-*   distributed under the License is distributed on an "AS IS" BASIS,
-*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*   See the License for the specific language governing permissions and
-*   limitations under the License.
-*/
+ *   Copyright (c) 2018, EPFL/Human Brain Project PCO
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 
 package services
 
+import akka.Done
 import play.api.Logger
 import play.api.cache.AsyncCacheApi
 
@@ -24,18 +25,23 @@ import scala.reflect.ClassTag
 
 trait CacheService {
 
-  val logger = Logger(this.getClass)
-  def getOrElse[A:ClassTag](cache : AsyncCacheApi, key: String)(orElse: => Future[Option[A]])(implicit executionContext: ExecutionContext):Future[Option[A]] = {
-    cache.get[A](key).flatMap{
-      case Some(elem) =>
-        Future(Some(elem))
-      case _ =>
-       orElse
+  val log = Logger(this.getClass)
+
+  def getOrElse[A: ClassTag](cache: AsyncCacheApi, key: String)(
+    orElse: => Future[Option[A]]
+  )(implicit executionContext: ExecutionContext): Future[Option[A]] = {
+    get[A](cache, key).flatMap {
+      case Some(elem) => Future(Some(elem))
+      case None =>
+        log.debug("Cache element not found executing orElse")
+        orElse
     }
   }
 
-  def get[A:ClassTag](cache : AsyncCacheApi, key: String)(implicit executionContext: ExecutionContext):Future[Option[A]] = {
-    cache.get[A](key)
-  }
+  def get[A: ClassTag](cache: AsyncCacheApi, key: String)(
+    implicit executionContext: ExecutionContext
+  ): Future[Option[A]] = cache.get[A](key)
+
+  def clearCache(cache: AsyncCacheApi): Future[Done] = cache.removeAll()
 
 }
