be5f16305c51987923ca3294f74575254b1d419c
David Kunzmann <davidkunzmann@gmail.com>
2019-01-10 11:53:51 +0100

    Creating metadata model

---
+++ modules/editor/app/models/instance/EditorMetadata.scala
@@ -0,0 +1,43 @@
+/*
+ *   Copyright (c) 2019, EPFL/Human Brain Project PCO
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package models.instance
+
+import org.joda.time.DateTime
+import play.api.libs.json.{JsPath, JsString, JsValue, Writes}
+
+case class EditorMetadata(
+  lastUpdate: Option[DateTime],
+  creationDate: Option[DateTime],
+  lastUpdater: String,
+  numberOfEdits: Long
+)
+
+object EditorMetadata {
+
+  import play.api.libs.functional.syntax._
+
+  implicit val jodaDateWrites: Writes[DateTime] = new Writes[DateTime] {
+    def writes(d: DateTime): JsValue = JsString(d.toString())
+  }
+  implicit val responseWithMetaDataWrites: Writes[EditorMetadata] = (
+    (JsPath \ "lastUpdate").writeNullable[DateTime] and
+    (JsPath \ "creationDate").writeNullable[DateTime] and
+    (JsPath \ "lastUpdater").write[String] and
+    (JsPath \ "numberOfEdits").write[Long]
+  )(unlift(EditorMetadata.unapply))
+
+  def empty: EditorMetadata = EditorMetadata(None, None, "", 0)
+}
