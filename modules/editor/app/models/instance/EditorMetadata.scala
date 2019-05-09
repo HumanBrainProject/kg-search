17a19f8d74144859ad1d8a1a2dfcabc11ba2b859
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2019-05-09 08:52:02 +0200

    metadata: converts metadata to object with label and value

--- modules/editor/app/models/instance/EditorMetadata.scala
+++ modules/editor/app/models/instance/EditorMetadata.scala
@@ -16,7 +16,8 @@
 package models.instance
 
 import org.joda.time.DateTime
-import play.api.libs.json.{JsPath, JsString, JsValue, Writes}
+import org.joda.time.format.DateTimeFormat
+import play.api.libs.json.{JsObject, JsString, JsValue, Json, Writes, Reads, JsNull, Format, JsResult, JsSuccess}
 
 case class EditorMetadata(
   createdBy: String,
@@ -28,18 +29,42 @@ case class EditorMetadata(
 
 object EditorMetadata {
 
-  import play.api.libs.functional.syntax._
-
   implicit val jodaDateWrites: Writes[DateTime] = new Writes[DateTime] {
     def writes(d: DateTime): JsValue = JsString(d.toString())
   }
-  implicit val responseWithMetaDataWrites: Writes[EditorMetadata] = (
-    (JsPath \ "createdBy").write[String] and
-    (JsPath \ "createdAt").writeNullable[DateTime] and
-    (JsPath \ "lastUpdateBy").write[String] and
-    (JsPath \ "lastUpdateAt").writeNullable[DateTime] and
-    (JsPath \ "numberOfEdits").write[Long]
-  )(unlift(EditorMetadata.unapply))
+
+  implicit val jodaDateReads: Reads[DateTime] = new Reads[DateTime] {
+    def reads(d: JsValue): JsResult[DateTime ]= JsSuccess(DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
+      .parseDateTime(d.as[String]))
+  }
+
+  implicit def optionFormat[T: Format]: Format[Option[T]] = new Format[Option[T]]{
+    override def reads(json: JsValue): JsResult[Option[T]] = json.validateOpt[T]
+
+    override def writes(o: Option[T]): JsValue = o match {
+      case Some(t) ⇒ implicitly[Writes[T]].writes(t)
+      case None ⇒ JsNull
+    }
+  }
+
+  implicit val responseWithMetaDataWrites: Writes[EditorMetadata]= new Writes[EditorMetadata] {
+    def writes(e: EditorMetadata): JsValue = {
+      val valuesToBeAdded:List[(String, JsObject)] = List("lastUpdateAt" -> Json.obj("label"->"Last update at", "value"->e.lastUpdateAt),
+        "createdAt" -> Json.obj("label"->"Created at", "value"->e.createdAt))
+      val initialValues:JsObject = Json.obj(
+        "createdBy" -> Json.obj("label"->"Created by", "value"->e.createdBy),
+        "lastUpdateBy" -> Json.obj("label"->"Last update by", "value"->e.lastUpdateBy),
+        "numberOfEdits" -> Json.obj("label"->"Number of edits", "value"->e.numberOfEdits)
+      )
+      valuesToBeAdded.foldLeft(initialValues){
+        case(acc, value) =>  if(value._2.value.get("value").map(_.as[Option[DateTime]]).getOrElse(None).isEmpty) {
+          acc
+        } else {
+          acc + value
+        }
+      }
+    }
+  }
 
   def empty: EditorMetadata = EditorMetadata("", None, "", None, 0)
 }
