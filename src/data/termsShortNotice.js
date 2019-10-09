7007e58e4127b07f1441aaf80ae1328a2b4c2a96
Oliver Schmid <oliver.schmid@epfl.ch>
2019-10-09 21:02:35 +0200

    Update termsShortNotice.js

--- src/data/termsShortNotice.js
+++ src/data/termsShortNotice.js
@@ -1,9 +1,12 @@
-export const termsShortNotice = `Access to the data and metadata provided through HBP Knowledge Graph Data Platform (&quot;KG&quot;) requires that you cite and acknowledge said data and metadata according to the Terms and Conditions of the Platform.
+export const termsShortNotice = `
+Our site saves small pieces of text information (cookies) on your device in order to deliver better content and for statistical purposes. You can disable the usage of cookies by changing the settings of your browser. By browsing our website without changing the browser settings you grant us permission to store that information on your device.
 
-Citation requirements are outlined [here](https://www.humanbrainproject.eu/en/explore-the-brain/search-terms-of-use#citations).
+Access to the data and metadata provided through EBRAINS Knowledge Graph Data Platform (&quot;KG&quot;) requires that you cite and acknowledge said data and metadata according to the Terms and Conditions of the Platform.
 
-Acknowledgement requirements are outlined [here](https://www.humanbrainproject.eu/en/explore-the-brain/search-terms-of-use#acknowledgements).
+Citation requirements are outlined [here](https://kg.ebrains.eu/search/search-terms-of-use#citations).
 
-These outlines are based on the authoritative Terms and Conditions are found [here](https://www.humanbrainproject.eu/en/explore-the-brain/search-terms-of-use).
+Acknowledgement requirements are outlined [here](https://kg.ebrains.eu/search/search-terms-of-use#acknowledgements).
+
+These outlines are based on the authoritative Terms and Conditions are found [here](https://kg.ebrains.eu/search/search-terms-of-use).
 
 If you do not accept the Terms &amp; Conditions you are not permitted to access or use the KG to search for, to submit, to post, or to download any materials found there-in.`;
