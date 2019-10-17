06c0d072ae6e45025f79be979ae9edfe90f78f3c
Oliver Schmid <oliver.schmid@epfl.ch>
2019-10-17 11:03:25 +0200

    slightly change appearance of ToU and cookies disclaimer

--- src/data/termsShortNotice.js
+++ src/data/termsShortNotice.js
@@ -1,12 +1,15 @@
 export const termsShortNotice = `
-Our site saves small pieces of text information (cookies) on your device in order to deliver better content and for statistical purposes. You can disable the usage of cookies by changing the settings of your browser. By browsing our website without changing the browser settings you grant us permission to store that information on your device.
-
 Access to the data and metadata provided through EBRAINS Knowledge Graph Data Platform (&quot;KG&quot;) requires that you cite and acknowledge said data and metadata according to the Terms and Conditions of the Platform.
 
-Citation requirements are outlined [here](https://kg.ebrains.eu/search-terms-of-use.html#citations).
+- [Citation requirements](https://kg.ebrains.eu/search-terms-of-use.html#citations)
 
-Acknowledgement requirements are outlined [here](https://kg.ebrains.eu/search-terms-of-use.html#acknowledgements).
+- [Acknowledgement requirements](https://kg.ebrains.eu/search-terms-of-use.html#acknowledgements)
 
-These outlines are based on the authoritative Terms and Conditions are found [here](https://kg.ebrains.eu/search-terms-of-use.html).
+- These outlines are based on the authoritative [Terms and Conditions](https://kg.ebrains.eu/search-terms-of-use.html)
 
-If you do not accept the Terms &amp; Conditions you are not permitted to access or use the KG to search for, to submit, to post, or to download any materials found there-in.`;
+If you do not accept the Terms &amp; Conditions you are not permitted to access or use the KG to search for, to submit, to post, or to download any materials found there-in.
+   
+   
+Our site saves small pieces of text information (cookies) on your device in order to deliver better content and for statistical purposes. You can disable the usage of cookies by changing the settings of your browser. By browsing our website without changing the browser settings you grant us permission to store that information on your device.
+
+`;
