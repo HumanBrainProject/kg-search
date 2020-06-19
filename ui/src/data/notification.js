46cb8643fbf800aaeb459bc678003c3ee0e810cf
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2020-06-19 13:09:25 +0200

    remove notification

--- ui/src/data/notification.js
+++ ui/src/data/notification.js
@@ -1,4 +1,2 @@
 
-export const notification = `
-##### An issue with the Knowledge Graph's automated ingestion of references to data files has been identified. We are in the process of resolving this. In the meantime, the list of files might not be complete. #####
-`;
+export const notification = "";
