3a81154229e599f0191f9f2c5bdc7691e571aae6
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2020-07-08 11:38:53 +0200

    publications fix for dataset

--- service/app/models/templates/instance/DatasetTemplate.scala
+++ service/app/models/templates/instance/DatasetTemplate.scala
@@ -172,7 +172,7 @@ trait DatasetTemplate extends Template with FileProxy {
             case Some(ObjectWithValueField(Some(doiStr))) =>
               val url = URLEncoder.encode(doiStr, "UTF-8")
               Some(ObjectWithValueField(Some(s"[DOI: $doiStr]\n[DOI: $doiStr]: https://doi.org/$url")))
-            case doi => doi
+            case _ => None
           }
         ),
         (citation, doi) => {
@@ -182,8 +182,11 @@ trait DatasetTemplate extends Template with FileProxy {
               Some(ObjectWithValueField(Some(doiStr: String)))
               ) =>
               Some(ObjectWithValueField[String](Some(citationStr + "\n" + doiStr)))
-            case _ => doi
+            case (Some(ObjectWithValueField(Some(citationStr: String))), _) =>Some(ObjectWithValueField[String](Some(citationStr)))
+            case (_, Some(ObjectWithValueField(Some(doiStr: String)))) =>Some(ObjectWithValueField[String](Some(doiStr)))
+            case _ => None
           }
+
         }
       )
     ),
