d6c42253fe0b17785b66f61b58c606527e7fb229
gdenerva <gilles.denervaud@gmail.com>
2020-07-14 09:21:26 +0200

    proxy

--- ui/src/setupProxy.js
+++ ui/src/setupProxy.js
@@ -21,12 +21,14 @@ module.exports = function(app) {
     //target: "https://kg.ebrains.eu",
     target: "https://kg-int.humanbrainproject.eu",
     //target: "https://kg-dev.humanbrainproject.eu",
+    // target:"http://localhost:9000",
     changeOrigin: true,
     ws: true }));
   app.use(proxy("/proxy", {
     //target: "https://kg.ebrains.eu",
     target: "https://kg-int.humanbrainproject.eu",
     //target: "https://kg-dev.humanbrainproject.eu",
+    // target:"http://localhost:9000",
     changeOrigin: true,
     ws: true }));
 };
