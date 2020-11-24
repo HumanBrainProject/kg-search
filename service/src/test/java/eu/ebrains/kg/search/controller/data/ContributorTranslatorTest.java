75ab344544ad33376ae9b14446e4826b1ca2a1c6
Gilles Dénervaud <gilles.denervaud@gmail.com>
2020-11-24 15:05:50 +0100

    contributor tests

---
+++ service/src/test/java/eu/ebrains/kg/search/controller/data/ContributorTranslatorTest.java
@@ -0,0 +1,78 @@
+package eu.ebrains.kg.search.controller.data;
+
+import eu.ebrains.kg.search.controller.utils.TranslatorTestHelper;
+import eu.ebrains.kg.search.model.DatabaseScope;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ContributorTranslatorTest {
+    @Test
+    public void compareReleasedContributorV1() throws IOException {
+        String sourceJson = IOUtils.toString(this.getClass().getResourceAsStream("/v1/personReleasedSource.json"), StandardCharsets.UTF_8);
+        List<String> sourcesJson = new ArrayList<>(Arrays.asList(sourceJson));
+        String expectedJson = IOUtils.toString(this.getClass().getResourceAsStream("/v2/contributorV1ReleasedTarget.json"), StandardCharsets.UTF_8);
+        List<String> result = TranslatorTestHelper.compareContributor(sourcesJson, expectedJson, DatabaseScope.RELEASED, false);
+        if (!result.isEmpty()) {
+            Assert.fail("\n\t" + String.join("\n\t", result));
+        }
+    }
+    @Test
+    public void compareReleasedContributorV2() throws IOException {
+        String sourceJson = IOUtils.toString(this.getClass().getResourceAsStream("/v2/personReleasedSource.json"), StandardCharsets.UTF_8);
+        List<String> sourcesJson = new ArrayList<>(Arrays.asList(null, sourceJson));
+        String expectedJson = IOUtils.toString(this.getClass().getResourceAsStream("/v2/contributorV2ReleasedTarget.json"), StandardCharsets.UTF_8);
+        List<String> result = TranslatorTestHelper.compareContributor(sourcesJson, expectedJson, DatabaseScope.RELEASED, false);
+        if (!result.isEmpty()) {
+            Assert.fail("\n\t" + String.join("\n\t", result));
+        }
+    }
+
+    @Test
+    public void compareInferredContributorV1() throws IOException {
+        String sourceJson = IOUtils.toString(this.getClass().getResourceAsStream("/v1/personInferredSource.json"), StandardCharsets.UTF_8);
+        List<String> sourcesJson = new ArrayList<>(Arrays.asList(sourceJson));
+        String expectedJson = IOUtils.toString(this.getClass().getResourceAsStream("/v2/contributorV1InferredTarget.json"), StandardCharsets.UTF_8);
+        List<String> result = TranslatorTestHelper.compareContributor(sourcesJson, expectedJson, DatabaseScope.INFERRED, false);
+        if (!result.isEmpty()) {
+            Assert.fail("\n\t" + String.join("\n\t", result));
+        }
+    }
+    @Test
+    public void compareInferredContributorV2() throws IOException {
+        String sourceJson = IOUtils.toString(this.getClass().getResourceAsStream("/v2/personInferredSource.json"), StandardCharsets.UTF_8);
+        List<String> sourcesJson = new ArrayList<>(Arrays.asList(null, sourceJson));
+        String expectedJson = IOUtils.toString(this.getClass().getResourceAsStream("/v2/contributorV2InferredTarget.json"), StandardCharsets.UTF_8);
+        List<String> result = TranslatorTestHelper.compareContributor(sourcesJson, expectedJson, DatabaseScope.INFERRED, false);
+        if (!result.isEmpty()) {
+            Assert.fail("\n\t" + String.join("\n\t", result));
+        }
+    }
+
+    @Test
+    public void compareInferredLiveContributorV1() throws IOException {
+        String sourceJson = IOUtils.toString(this.getClass().getResourceAsStream("/v1/personInferredSource.json"), StandardCharsets.UTF_8);
+        List<String> sourcesJson = new ArrayList<>(Arrays.asList(sourceJson));
+        String expectedJson = IOUtils.toString(this.getClass().getResourceAsStream("/v2/contributorV1InferredTarget.json"), StandardCharsets.UTF_8);
+        List<String> result = TranslatorTestHelper.compareContributor(sourcesJson, expectedJson, DatabaseScope.INFERRED, true);
+        if (!result.isEmpty()) {
+            Assert.fail("\n\t" + String.join("\n\t", result));
+        }
+    }
+    @Test
+    public void compareInferredLiveContributorV2() throws IOException {
+        String sourceJson = IOUtils.toString(this.getClass().getResourceAsStream("/v2/personInferredSource.json"), StandardCharsets.UTF_8);
+        List<String> sourcesJson = new ArrayList<>(Arrays.asList(null, sourceJson));
+        String expectedJson = IOUtils.toString(this.getClass().getResourceAsStream("/v2/contributorV2InferredLiveTarget.json"), StandardCharsets.UTF_8);
+        List<String> result = TranslatorTestHelper.compareContributor(sourcesJson, expectedJson, DatabaseScope.INFERRED, true);
+        if (!result.isEmpty()) {
+            Assert.fail("\n\t" + String.join("\n\t", result));
+        }
+    }
+}
--- service/src/test/java/eu/ebrains/kg/search/controller/utils/TranslatorTestHelper.java
+++ service/src/test/java/eu/ebrains/kg/search/controller/utils/TranslatorTestHelper.java
@@ -1,10 +1,16 @@
 package eu.ebrains.kg.search.controller.utils;
 
+import eu.ebrains.kg.search.controller.translators.ContributorTranslator;
 import eu.ebrains.kg.search.controller.translators.ModelTranslator;
 import eu.ebrains.kg.search.controller.translators.SoftwareTranslator;
 import eu.ebrains.kg.search.model.DatabaseScope;
+import eu.ebrains.kg.search.model.source.PersonSources;
+import eu.ebrains.kg.search.model.source.PersonV1andV2;
+import eu.ebrains.kg.search.model.source.openMINDSv1.PersonV1;
 import eu.ebrains.kg.search.model.source.openMINDSv2.ModelV2;
+import eu.ebrains.kg.search.model.source.openMINDSv2.PersonV2;
 import eu.ebrains.kg.search.model.source.openMINDSv2.SoftwareV2;
+import eu.ebrains.kg.search.model.target.elasticsearch.instances.Contributor;
 import eu.ebrains.kg.search.model.target.elasticsearch.instances.Model;
 import eu.ebrains.kg.search.model.target.elasticsearch.instances.Software;
 
@@ -16,6 +22,32 @@ import static org.junit.Assert.assertEquals;
 
 public class TranslatorTestHelper {
     private static final JsonAdapter jsonAdapter = new JsonAdapter();
+
+    public static List<String> compareContributor(List<String> sourcesJson, String expectedJson, DatabaseScope databaseScope, boolean liveMode) {
+        ContributorTranslator translator = new ContributorTranslator();
+
+        PersonSources sources = new PersonSources();
+
+        String sourceJsonV1 = sourcesJson.get(0);
+        if (sourceJsonV1 != null) {
+            PersonV1 source = jsonAdapter.fromJson(sourceJsonV1, PersonV1.class);
+            sources.setPersonV1(source);
+        }
+
+        String sourceJsonV2 = sourcesJson.get(1);
+        if (sourceJsonV2 != null) {
+            PersonV2 source = jsonAdapter.fromJson(sourceJsonV2, PersonV2.class);
+            sources.setPersonV2(source);
+        }
+
+        Map<String, Object> targetExpected = jsonAdapter.fromJson(expectedJson, Map.class);
+
+        Contributor target = translator.translate(sources, databaseScope, liveMode);
+        String targetJson = jsonAdapter.toJson(target);
+        Map<String, Object> targetResult = jsonAdapter.fromJson(targetJson, Map.class);
+
+        return compareResults(targetExpected, targetResult);
+    }
 
     public static List<String> compareModel(String sourceJson, String expectedJson, DatabaseScope databaseScope, boolean liveMode) {
         ModelTranslator translator = new ModelTranslator();
