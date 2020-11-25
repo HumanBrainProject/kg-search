0331e5c69b6bd76070fdf22b9b76e6485700e68b
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2020-11-25 17:07:50 +0100

    software tests

--- service/src/test/java/eu/ebrains/kg/search/controller/data/SoftwareTranslatorTest.java
+++ service/src/test/java/eu/ebrains/kg/search/controller/data/SoftwareTranslatorTest.java
@@ -1,16 +1,41 @@
 package eu.ebrains.kg.search.controller.data;
 
 import eu.ebrains.kg.search.controller.utils.TranslatorTestHelper;
+import eu.ebrains.kg.search.controller.utils.WebClientHelper;
 import eu.ebrains.kg.search.model.DatabaseScope;
+import eu.ebrains.kg.search.model.source.ResultOfKGv2;
+import eu.ebrains.kg.search.model.source.openMINDSv1.SubjectV1;
+import eu.ebrains.kg.search.model.source.openMINDSv2.SoftwareV2;
+import eu.ebrains.kg.search.model.target.elasticsearch.ElasticSearchDocument;
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class SoftwareTranslatorTest {
+    private static class SoftwareV2Result extends ResultOfKGv2<SoftwareV2> { }
+
+    @Test
+    public void compareReleasedSoftwareAll() {
+        List<String> result = new ArrayList<>();
+        SoftwareV2Result queryResult = WebClientHelper.executeQuery("query/softwarecatalog/software/softwareproject/v1.0.0/search", DatabaseScope.RELEASED, SoftwareV2Result.class);
+        queryResult.getResults().forEach(software -> {
+            String id = software.getIdentifier();
+            Map<String, Object> expected = WebClientHelper.getDocument("public", "Software", id, ElasticSearchDocument.class).getSource();
+            List<String> messages = TranslatorTestHelper.compareSoftware(software, expected, DatabaseScope.RELEASED, false);
+            if (!messages.isEmpty()) {
+                result.add("\n\n\tSubject: " + id + "\n\t\t" + String.join("\n\t\t", messages));
+            }
+        });
+        if (!result.isEmpty()) {
+            Assert.fail(String.join("", result));
+        }
+    }
 
     @Test
     public void compareReleasedSoftware() throws IOException {
