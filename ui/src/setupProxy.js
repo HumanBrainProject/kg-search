d9b4b8a8fc35c9b049f900190a7b21599de9e314
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2021-01-07 14:28:50 +0100

    new-footer

--- ui/src/setupProxy.js
+++ ui/src/setupProxy.js
@@ -6,9 +6,9 @@ module.exports = function(app) {
     createProxyMiddleware({
       //target: "https://search.kg.ebrains.eu",
       // target: "https://search.kg-ppd.ebrains.eu",
-      //target: "https://search.kg-int.ebrains.eu",
+      // target: "https://search.kg-int.ebrains.eu",
       //target: "https://search.kg-dev.ebrains.eu",
-      target:"http://localhost:8080",
+      // target:"http://localhost:8080",
       secure:false,
       changeOrigin: true
     })
