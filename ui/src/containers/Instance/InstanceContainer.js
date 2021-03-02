fc7203d889f1074d446671a7e06726ee2e49be54
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2021-03-02 14:21:20 +0100

    change icon

--- ui/src/containers/Instance/InstanceContainer.js
+++ ui/src/containers/Instance/InstanceContainer.js
@@ -144,7 +144,7 @@ export class InstanceContainer extends React.Component {
           {showInstance && (
             <React.Fragment>
               <BackLinkButton />
-              <button className="kgs-container__backButton kgs-back__search" onClick={this.handleBackToSearch}><i className="fa fa-search"></i>&nbsp;Back to search</button>
+              <button className="kgs-container__backButton kgs-back__search" onClick={this.handleBackToSearch}><i className="fa fa-arrow-circle-left"></i>&nbsp;Back to search</button>
               <Instance {...this.props.instanceProps} NavigationComponent={NavigationComponent} fetch={fetch} />
             </React.Fragment>
           )}
