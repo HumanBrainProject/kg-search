6d92e3f46974c58d0c7289feb073b51ff3c1671a
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2021-03-03 14:39:34 +0100

    refactor uploadQuery
    Co-authored-by: Gilles Dénervaud <gilles.denervaud@gmail.com>

--- service/src/main/java/eu/ebrains/kg/search/services/KGV3ServiceClient.java
+++ service/src/main/java/eu/ebrains/kg/search/services/KGV3ServiceClient.java
@@ -6,12 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.Map;
 
@@ -22,7 +20,7 @@ public class KGV3ServiceClient {
     private final WebClient userWebClient;
     private final String kgCoreEndpoint;
 
-    public KGV3ServiceClient(@Qualifier("asServiceAccount") WebClient serviceAccountWebClient, @Qualifier("asUser") WebClient userWebClient,  @Value("${kgcore.endpoint}") String kgCoreEndpoint){
+    public KGV3ServiceClient(@Qualifier("asServiceAccount") WebClient serviceAccountWebClient, @Qualifier("asUser") WebClient userWebClient, @Value("${kgcore.endpoint}") String kgCoreEndpoint) {
         this.serviceAccountWebClient = serviceAccountWebClient;
         this.userWebClient = userWebClient;
         this.kgCoreEndpoint = kgCoreEndpoint;
@@ -44,29 +42,14 @@ public class KGV3ServiceClient {
     }
 
     public void uploadQuery(String queryId, String payload) {
-        try {
-            String url = String.format("%s/queries/%s", kgCoreEndpoint, queryId);
-            serviceAccountWebClient.put()
-                    .uri(url)
-                    .body(BodyInserters.fromValue(payload))
-                    .headers(h -> h.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                    .retrieve()
-                    .bodyToMono(Void.class)
-                    .block();
-        } catch (WebClientResponseException e) {
-            if(e.getStatusCode() == HttpStatus.BAD_REQUEST) {
-                String url = String.format("%s/queries/%s?space=kg-search", kgCoreEndpoint, queryId);
-                serviceAccountWebClient.put()
-                        .uri(url)
-                        .body(BodyInserters.fromValue(payload))
-                        .headers(h -> h.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                        .retrieve()
-                        .bodyToMono(Void.class)
-                        .block();
-            } else {
-                throw e;
-            }
-        }
+        String url = String.format("%s/queries/%s?space=kg-search", kgCoreEndpoint, queryId);
+        serviceAccountWebClient.put()
+                .uri(url)
+                .body(BodyInserters.fromValue(payload))
+                .headers(h -> h.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .retrieve()
+                .bodyToMono(Void.class)
+                .block();
     }
 
     private <T> T executeCallForLive(Class<T> clazz, String url) {
