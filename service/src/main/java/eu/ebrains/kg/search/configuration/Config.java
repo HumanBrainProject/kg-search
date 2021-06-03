79875deee493e6c45c00cf4e7c0f85efb183a28e
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2021-06-03 11:27:07 +0200

    add commit message
    Co-authored-by: Gilles Dénervaud <gilles.denervaud@gmail.com>

--- service/src/main/java/eu/ebrains/kg/search/configuration/Config.java
+++ service/src/main/java/eu/ebrains/kg/search/configuration/Config.java
@@ -63,16 +63,16 @@ public class Config {
 
 
     @Bean
-    public OpenAPI customOpenAPI(@Value("${eu.ebrains.kg.login.endpoint}") String loginEndpoint) {
+    public OpenAPI customOpenAPI(@Value("${eu.ebrains.kg.login.endpoint}") String loginEndpoint,  @Value("${eu.ebrains.kg.commit}") String commit) {
         OAuthFlow oAuthFlow = new OAuthFlow();
         oAuthFlow.authorizationUrl(loginEndpoint);
         SecurityScheme userToken = new SecurityScheme().name("Authorization").type(SecurityScheme.Type.OAUTH2).flows(new OAuthFlows().implicit(oAuthFlow)).description("The user authentication");
         SecurityRequirement userWithoutClientReq = new SecurityRequirement().addList("Authorization");
 
         OpenAPI openapi = new OpenAPI().openapi("3.0.3");
-        String description = "This is the API of the EBRAINS Knowledge Graph Search";
+        String description = String.format("This is the API of the EBRAINS Knowledge Graph Search (commit %s)", commit);
 
-        return openapi.info(new Info().version("v3.0.0").title(String.format("This is the EBRAINS KG Search API")).description(description).license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0.html")).termsOfService("https://kg.ebrains.eu/search-terms-of-use.html"))
+        return openapi.info(new Info().version("v3.0.0").title("This is the EBRAINS KG Search API").description(description).license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0.html")).termsOfService("https://kg.ebrains.eu/search-terms-of-use.html"))
                 .components(new Components()).schemaRequirement("Authorization", userToken)
                 .security(Collections.singletonList(userWithoutClientReq));
     }
