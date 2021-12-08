5bd82574cdd43e054d63594ead4f5b598288194c
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2021-12-08 13:40:25 +0100

    add small debounce
    Co-authored-by: Gilles Dénervaud <gilles.denervaud@gmail.com>

--- ui/src/components/Field/Files/HierarchicalFiles.js
+++ ui/src/components/Field/Files/HierarchicalFiles.js
@@ -35,9 +35,10 @@ import Header from "./Header";
 import { getTreeByFolder } from "./FileTreeByFolderHelper";
 import { getTreeByGroupingType } from "./FileTreeByGroupingTypeHelper";
 import * as filters from "./helpers";
+import { debounce } from "lodash";
 
 
-const Node = ({node, isRootNode, group}) => {
+const Node = ({node, isRootNode, group, hasFilter}) => {
   return (
     <div className="kgs-hierarchical-files__details">
       <div>{node.thumbnail ? <img height="80" src={node.thumbnail} alt={node.url} />:<FontAwesomeIcon icon={node.type} size="5x"/>}</div>
@@ -45,9 +46,10 @@ const Node = ({node, isRootNode, group}) => {
         <div>
           <div><strong>Name:</strong> {node.name}</div>
           {node.size  && <div><strong>Size:</strong> {node.size}</div>}
-          {node.url && (
-            <Download name={`Download ${isRootNode?"dataset":node.type}`} type={node.type} url={node.url} />
-          )}
+          {hasFilter && node.type === "folder" ? null:
+            node.url && (
+              <Download name={`Download ${isRootNode?"dataset":node.type}`} type={node.type} url={node.url} />
+            )}
           {node.type === "file" && node.details && node.details.data && node.details.mapping && (
             <File data={node.details.data} mapping={node.details.mapping} group={group} />
           )}
@@ -63,7 +65,8 @@ class HierarchicalFiles extends React.Component {
     this.state = {
       node: {},
       tree: {},
-      initialTree: {}
+      initialTree: {},
+      filter: ""
     };
   }
 
@@ -85,15 +88,16 @@ class HierarchicalFiles extends React.Component {
     this.setState({node: node});
   }
 
-  onFilterMouseUp = ({target: {value}}) => {
+  onFilterMouseUp = debounce(({target: {value}}) => {
     const filter = value.trim();
+    this.setState({filter: filter});
     if (!filter) {
       this.setState({tree: this.state.initialTree});
     }
     let filtered = filters.filterTree(this.state.initialTree, filter);
     filtered = filters.expandFilteredNodes(filtered, filter);
     this.setState({tree: filtered});
-  }
+  }, 150)
 
   render() {
     const filesLength = this.props.data && this.props.data.length;
@@ -117,7 +121,7 @@ class HierarchicalFiles extends React.Component {
             style={{...theme}}
           />
           {this.state.node.active && (
-            <Node node={this.state.node} isRootNode={this.state.node === this.state.tree} group={this.props.group} />
+            <Node node={this.state.node} isRootNode={this.state.node.isRootNode} group={this.props.group} hasFilter={this.state.filter !== ""} />
           )}
         </div>
       </>
