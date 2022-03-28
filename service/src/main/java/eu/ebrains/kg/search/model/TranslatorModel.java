ba819fc74bac658ef04abeeb073b3adeeb5c050d
Oliver Schmid <oliver.schmid@ebrains.eu>
2022-03-28 21:46:50 +0200

    customize bulk size

--- service/src/main/java/eu/ebrains/kg/search/model/TranslatorModel.java
+++ service/src/main/java/eu/ebrains/kg/search/model/TranslatorModel.java
@@ -17,24 +17,24 @@ import java.util.List;
 public class TranslatorModel<v1Input, v2Input, v3Input, Output extends TargetInstance> {
 
     public static final List<TranslatorModel<?,?,?,?>> MODELS = Arrays.asList(
-            new TranslatorModel<>(Project.class, new ProjectV1Translator(), null, new ProjectV3Translator(), new ProjectMerger(), false, false),
-            new TranslatorModel<>(Dataset.class, null, null, new DatasetV3Translator(), null, false, false),
-            new TranslatorModel<>(DatasetVersion.class, new DatasetV1Translator(), null, new DatasetVersionV3Translator(), null, false, false),
-            new TranslatorModel<>(Subject.class, new SubjectV1Translator(), null, new SubjectV3Translator(), null, false, false),
-            new TranslatorModel<>(Sample.class, new SampleV1Translator(), null, null, null, false, false),
-            new TranslatorModel<>(ModelVersion.class, null, null, new ModelVersionV3Translator(), null, false, false),
-            new TranslatorModel<>(MetaDataModel.class, null, null, new MetaDataModelV3Translator(), null, false, false),
-            new TranslatorModel<>(MetaDataModelVersion.class, null, null, new MetaDataModelVersionV3Translator(), null, false, false),
-            new TranslatorModel<>(SoftwareVersion.class, null, null, new SoftwareVersionV3Translator(), null, false, true),
-            new TranslatorModel<>(Model.class, null, null, new ModelV3Translator(), null, false, false),
-            new TranslatorModel<>(Software.class, null, null, new SoftwareV3Translator(), null, false, false),
-            new TranslatorModel<>(Contributor.class, new PersonV1Translator(), new PersonV2Translator(), new ContributorV3Translator(), new ContributorMerger(), false, false),
-            new TranslatorModel<>(ControlledTerm.class, null, null, new ControlledTermV3Translator(), null, false, false),
-            new TranslatorModel<>(ContentType.class, null, null, new ContentTypeV3Translator(), null, false, false),
-            new TranslatorModel<>(File.class, null, null, new FileV3Translator(), null, true, false),
-            new TranslatorModel<>(FileBundle.class, null, null, new FileBundleV3Translator(), null, true, false),
-            new TranslatorModel<>(ParcellationEntity.class, null, null, new ParcellationEntityV3Translator(), null, false, false),
-            new TranslatorModel<>(BehavioralProtocol.class, null, null, new BehavioralProtocolV3Translator(), null, false, false)
+            new TranslatorModel<>(Project.class, new ProjectV1Translator(), null, new ProjectV3Translator(), new ProjectMerger(), false, false, 1000),
+            new TranslatorModel<>(Dataset.class, null, null, new DatasetV3Translator(), null, false, false, 1000),
+            new TranslatorModel<>(DatasetVersion.class, new DatasetV1Translator(), null, new DatasetVersionV3Translator(), null, false, false, 100),
+            new TranslatorModel<>(Subject.class, new SubjectV1Translator(), null, new SubjectV3Translator(), null, false, false, 1000),
+            new TranslatorModel<>(Sample.class, new SampleV1Translator(), null, null, null, false, false, 1000),
+            new TranslatorModel<>(ModelVersion.class, null, null, new ModelVersionV3Translator(), null, false, false, 1000),
+            new TranslatorModel<>(MetaDataModel.class, null, null, new MetaDataModelV3Translator(), null, false, false, 1000),
+            new TranslatorModel<>(MetaDataModelVersion.class, null, null, new MetaDataModelVersionV3Translator(), null, false, false, 1000),
+            new TranslatorModel<>(SoftwareVersion.class, null, null, new SoftwareVersionV3Translator(), null, false, true, 1000),
+            new TranslatorModel<>(Model.class, null, null, new ModelV3Translator(), null, false, false, 1000),
+            new TranslatorModel<>(Software.class, null, null, new SoftwareV3Translator(), null, false, false, 1000),
+            new TranslatorModel<>(Contributor.class, new PersonV1Translator(), new PersonV2Translator(), new ContributorV3Translator(), new ContributorMerger(), false, false, 1000),
+            new TranslatorModel<>(ControlledTerm.class, null, null, new ControlledTermV3Translator(), null, false, false, 1000),
+            new TranslatorModel<>(ContentType.class, null, null, new ContentTypeV3Translator(), null, false, false, 1000),
+            new TranslatorModel<>(File.class, null, null, new FileV3Translator(), null, true, false, 1000),
+            new TranslatorModel<>(FileBundle.class, null, null, new FileBundleV3Translator(), null, true, false, 1000),
+            new TranslatorModel<>(ParcellationEntity.class, null, null, new ParcellationEntityV3Translator(), null, false, false, 1000),
+            new TranslatorModel<>(BehavioralProtocol.class, null, null, new BehavioralProtocolV3Translator(), null, false, false, 1000)
     );
 
     public static TranslatorModel<?,?,?,?> getModelByType(String type){
@@ -49,8 +49,9 @@ public class TranslatorModel<v1Input, v2Input, v3Input, Output extends TargetIns
     private final Merger<Output> merger;
     private final boolean autoRelease;
     private final boolean onlyV3ForInProgress;
+    private final int bulkSize;
 
-    private TranslatorModel(Class<Output> targetClass, Translator<v1Input, Output, ? extends ResultsOfKGv2<v1Input>> v1translator, Translator<v2Input, Output, ? extends ResultsOfKGv2<v2Input>> v2translator, TranslatorV3<v3Input, Output, ? extends ResultsOfKGv3<v3Input>> v3Translator, Merger<Output> merger, boolean autoRelease, boolean onlyV3ForInProgress) {
+    private TranslatorModel(Class<Output> targetClass, Translator<v1Input, Output, ? extends ResultsOfKGv2<v1Input>> v1translator, Translator<v2Input, Output, ? extends ResultsOfKGv2<v2Input>> v2translator, TranslatorV3<v3Input, Output, ? extends ResultsOfKGv3<v3Input>> v3Translator, Merger<Output> merger, boolean autoRelease, boolean onlyV3ForInProgress, int bulkSize) {
         this.targetClass = targetClass;
         this.v1translator = v1translator;
         this.v2translator = v2translator;
@@ -58,6 +59,7 @@ public class TranslatorModel<v1Input, v2Input, v3Input, Output extends TargetIns
         this.merger = merger;
         this.autoRelease = autoRelease;
         this.onlyV3ForInProgress = onlyV3ForInProgress;
+        this.bulkSize = bulkSize;
     }
 
     public boolean isAutoRelease() {
@@ -90,12 +92,12 @@ public class TranslatorModel<v1Input, v2Input, v3Input, Output extends TargetIns
     }
 
     public int getBulkSize() {
-        //TODO handle the "big" source models by restraining the bulk size
-        return 1000;
+        return bulkSize;
     }
 
     public int getBulkSizeV2() {
         //TODO handle the "big" source models by restraining the bulk size
         return 200;
     }
+
 }
