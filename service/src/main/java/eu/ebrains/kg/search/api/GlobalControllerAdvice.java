9c20a1a33c9e60328e9d1e957c0b95d3ac7621cf
Oliver Schmid <oliver.schmid@ebrains.eu>
2022-03-31 11:56:46 +0200

    apply temporary fix for 0-day exploit

---
+++ service/src/main/java/eu/ebrains/kg/search/api/GlobalControllerAdvice.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022 EPFL/Human Brain Project PCO
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.ebrains.kg.search.api;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+@Order(10000)
+public class GlobalControllerAdvice {
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        // This code protects Spring Core from a "Remote Code Execution" attack (dubbed "Spring4Shell").
+        // By applying this mitigation, you prevent the "Class Loader Manipulation" attack vector from firing.
+        // For more details, see this post: https://www.lunasec.io/docs/blog/spring-rce-vulnerabilities/
+        String[] denylist = new String[]{"class.*", "Class.*", "*.class.*", "*.Class.*"};
+        dataBinder.setDisallowedFields(denylist);
+    }
+}
