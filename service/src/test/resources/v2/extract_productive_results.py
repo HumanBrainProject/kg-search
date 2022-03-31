d16a757dd1f19b65dcefd28dbb922b1cb322450b
gdenervaud <gilles.denervaud@gmail.com>
2022-03-31 11:21:34 +0200

    remove deprecated tests
    Co-authored-by: Ioannis Tsanaktsidis <tsanakts.10@gmail.com>

--- service/src/test/resources/v2/extract_productive_results.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("personSourceSample.jsonld") as file:
-    payload = json.load(file)
-    for result in payload["results"]:
-        print(result["https://schema.hbp.eu/search/identifier"])
