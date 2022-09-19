ce88c403387652e54cb9cdbeec735814d08d89e7
gdenervaud <gilles.denervaud@gmail.com>
2022-09-19 08:01:51 +0200

    fix test

--- ui/src/pages/App.test.js
+++ ui/src/pages/App.test.js
@@ -24,7 +24,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import store from "../../app/store";
+import store from ".. /app/store";
 import App from "./App";
 
 // eslint-disable-next-line jest/expect-expect
