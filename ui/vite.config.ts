c52bc14cfb3c6fbe5bae4ff81b3e687c6f557b1f
Oliver Schmid <oliver.schmid@cscs.ch>
2024-02-21 11:31:35 +0100

    add prototypical tef-health style incl. theme toggle

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,7 +3,7 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default {
     // depending on your application, base can also be "/"
-    base: '',
+    base: '/',
     plugins: [react(), viteTsconfigPaths()],
     server: {
         // this ensures that the browser opens upon server start
