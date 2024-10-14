e17d63683fb258591db48016fc82709f5e72b11b
Oliver Schmid <oliver.schmid@cscs.ch>
2024-10-14 11:01:14 +0200

    change copyright notice for tef-health

--- ui/src/data/profiles.jsx
+++ ui/src/data/profiles.jsx
@@ -4,6 +4,7 @@ const profiles = {
         "name": "",
         "copyright": "",
         "copyrightSince": "",
+        "copyrightAddition": null,
         "searchExample": "",
         "logo": "",
         "logo_dark": ""
@@ -13,6 +14,7 @@ const profiles = {
         "name": "EBRAINS",
         "copyright": "ebrains.eu",
         "copyrightSince": "2019",
+        "copyrightAddition": null,
         "searchExample": "(e.g. mouse hippocampus or calbindin)",
         "logo": "ebrains_logo.svg",
         "logo_dark": "ebrains_logo_dark.svg"
@@ -20,7 +22,8 @@ const profiles = {
     "tefhealth": {
         "home": "https://tefhealth.eu",
         "name": "TEF-Health",
-        "copyright": "TEF-Health consortium",
+        "copyright": "TEF-Health. All rights reserved.",
+        "copyrightAddition": "The content on this website, including text, graphics, and images, is protected by copyright and other intellectual property laws. You may not reproduce, distribute, or modify any content without explicit permission. Any unauthorized use may result in legal action.",
         "copyrightSince": "2024",
         "searchExample": "(e.g. imaging or operating room)",
         "logo": "tefhealth_logo.png",
--- ui/src/pages/Footer/Footer.jsx
+++ ui/src/pages/Footer/Footer.jsx
@@ -28,105 +28,112 @@ import './Footer.css';
 import profiles from "../../data/profiles";
 
 const Footer = ({ commit, profile, theme }) => (
-  <footer className="site-footer">
-    <div className="footer__header">
-      <div className="footer__primary">
+    <footer className="site-footer">
+      <div className="footer__header">
+        <div className="footer__primary">
 
 
           {profile !== 'ebrains' &&
-              <span className="service-at"><a href={profiles[profile]["home"]} aria-label="Homepage" title="Homepage" className="logo nuxt-link-exact-active nuxt-link-active"> <img src={`/static/img/${theme === 'dark' ? profiles[profile]["logo_dark"] : profiles[profile]["logo"]}`}
-                                                                                                                                                              height="100" /></a> @ </span>}
-                <a href="https://ebrains.eu" aria-label="Ebrains homepage" title="Ebrains homepage" className="logo nuxt-link-exact-active nuxt-link-active"><img
-                    src={`/static/img/${theme === 'dark' ? "ebrains_logo_dark.svg" : 'ebrains_logo.svg'}`}
-                    height="100"/>
-                </a>
-              </div>
-            </div>
-            <div className="footer__content">
-            <div className="footer__secondary">
-            <p>EBRAINS is powered by the EU-cofunded Human Brain Project</p>
-        <div className="powered-by">
-          <a className="hbp-logo" href="https://www.humanbrainproject.eu/" title="Human Brain Project" target="_blank" rel="noopener noreferrer">
-            <img src="/static/img/hbp.svg" alt="Human Brain Project" />
-          </a>
-          <a className="eu-logo" href="https://ec.europa.eu/programmes/horizon2020/en/h2020-section/fet-flagships" title="Cofunded by EU"  target="_blank" rel="noopener noreferrer">
-            <img src="/static/img/eu_cofunded_logo.png" alt="Cofunded by EU" />
+              <span className="service-at"><a href={profiles[profile]["home"]} aria-label="Homepage" title="Homepage"
+                                              className="logo nuxt-link-exact-active nuxt-link-active"> <img
+                  src={`/static/img/${theme === 'dark' ? profiles[profile]["logo_dark"] : profiles[profile]["logo"]}`}
+                  height="100"/></a> @ </span>}
+          <a href="https://ebrains.eu" aria-label="Ebrains homepage" title="Ebrains homepage"
+             className="logo nuxt-link-exact-active nuxt-link-active"><img
+              src={`/static/img/${theme === 'dark' ? "ebrains_logo_dark.svg" : 'ebrains_logo.svg'}`}
+              height="100"/>
           </a>
         </div>
-              {profile==="ebrains" && <ThemeToggle />}
       </div>
-      <div className="footer__menu footer__menu-1">
-        <ul>
-          <li className="title">
-            <a href="https://ebrains.eu/services">Services</a>
-          </li>
-          <li>
-            <a href="https://ebrains.eu/services/data-and-knowledge">Data and Knowledge</a>
-          </li>
-          <li>
-            <a href="https://ebrains.eu/services/atlases">Atlases</a>
-          </li>
-          <li>
-            <a href="https://ebrains.eu/services/simulation">Simulation</a>
-          </li>
-          <li>
-            <a href="https://ebrains.eu/services/brain-inspired-technologies">Brain-Inspired Technologies</a>
-          </li>
-          <li>
-            <a href="https://ebrains.eu/services/medical-data">Medical Data Analytics</a>
-          </li>
-        </ul>
+      <div className="footer__content">
+        <div className="footer__secondary">
+          <p>EBRAINS is powered by the EU-cofunded Human Brain Project</p>
+          <div className="powered-by">
+            <a className="hbp-logo" href="https://www.humanbrainproject.eu/" title="Human Brain Project" target="_blank"
+               rel="noopener noreferrer">
+              <img src="/static/img/hbp.svg" alt="Human Brain Project"/>
+            </a>
+            <a className="eu-logo" href="https://ec.europa.eu/programmes/horizon2020/en/h2020-section/fet-flagships"
+               title="Cofunded by EU" target="_blank" rel="noopener noreferrer">
+              <img src="/static/img/eu_cofunded_logo.png" alt="Cofunded by EU"/>
+            </a>
+          </div>
+          {profile === "ebrains" && <ThemeToggle/>}
+        </div>
+        <div className="footer__menu footer__menu-1">
+          <ul>
+            <li className="title">
+              <a href="https://ebrains.eu/services">Services</a>
+            </li>
+            <li>
+              <a href="https://ebrains.eu/services/data-and-knowledge">Data and Knowledge</a>
+            </li>
+            <li>
+              <a href="https://ebrains.eu/services/atlases">Atlases</a>
+            </li>
+            <li>
+              <a href="https://ebrains.eu/services/simulation">Simulation</a>
+            </li>
+            <li>
+              <a href="https://ebrains.eu/services/brain-inspired-technologies">Brain-Inspired Technologies</a>
+            </li>
+            <li>
+              <a href="https://ebrains.eu/services/medical-data">Medical Data Analytics</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__menu footer__menu-2">
+          <ul>
+            <li className="title">Contact us</li>
+            <li>
+              <a href="https://ebrains.eu/support">Support</a>
+            </li>
+            <li>
+              <a href="https://ebrains.eu/register">Register</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__menu footer__menu-3">
+          <ul>
+            <li className="title">
+              <a href="https://ebrains.eu/about">About EBRAINS</a>
+            </li>
+            <li>
+              <a href="https://ebrains.eu/terms">Terms and Policies</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="footer__menu footer__menu-2">
-        <ul>
-          <li className="title">Contact us</li>
+      <hr className="full-width"/>
+      <div className="footer__end">
+        <div className="footer__copyright">
+          &copy;{profiles[profile]["copyrightSince"] !== new Date().getFullYear().toString() && profiles[profile]["copyrightSince"] + '-'}{new Date().getFullYear()}&nbsp;{profiles[profile]["copyright"]}
+        </div>
+        <div className="commit">
+          {commit && <span>build: <i>{commit}</i></span>}
+        </div>
+        <ul className="footer__social">
           <li>
-            <a href="https://ebrains.eu/support">Support</a>
+            <a href="https://www.facebook.com/Ebrains_eu-112559066821827" target="_blank" rel="noopener noreferrer"
+               title="Facebook">
+              <svg className="sprite-icons" xmlns="http://www.w3.org/2000/svg" id="i-facebook" viewBox="0 0 40 40"
+                   fill="none">
+                <path d="M26 10h-3a5 5 0 00-5 5v3h-3v4h3v8h4v-8h3l1-4h-4v-3a1 1 0 011-1h3v-4z" stroke="currentColor"
+                      strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
+            </a>
           </li>
           <li>
-            <a href="https://ebrains.eu/register">Register</a>
+            <a href="https://twitter.com/ebrains_eu" target="_blank" rel="noopener noreferrer" title="Twitter">
+              <svg className="sprite-icons" xmlns="http://www.w3.org/2000/svg" id="i-twitter" viewBox="0 0 40 40"
+                   fill="none">
+                <path
+                    d="M31 11a10.897 10.897 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 0111 12s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5 0-.278-.028-.556-.08-.83A7.72 7.72 0 0031 11v0z"
+                    stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
+            </a>
           </li>
-        </ul>
-      </div>
-      <div className="footer__menu footer__menu-3">
-        <ul>
-          <li className="title">
-            <a href="https://ebrains.eu/about">About EBRAINS</a>
-          </li>
-          <li>
-            <a href="https://ebrains.eu/terms">Terms and Policies</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <hr className="full-width" />
-    <div className="footer__end">
-      <div className="footer__copyright">
-        &copy;{profiles[profile]["copyrightSince"] !== new Date().getFullYear().toString() && profiles[profile]["copyrightSince"]+'-'}{new Date().getFullYear()}&nbsp;{profiles[profile]["copyright"]}
-      </div>
-      <div className="commit">
-        {commit && <span>build: <i>{commit}</i></span>}
-      </div>
-      <ul className="footer__social">
-        <li>
-          <a href="https://www.facebook.com/Ebrains_eu-112559066821827" target="_blank" rel="noopener noreferrer" title="Facebook">
-            <svg className="sprite-icons" xmlns="http://www.w3.org/2000/svg" id="i-facebook" viewBox="0 0 40 40"
-              fill="none">
-              <path d="M26 10h-3a5 5 0 00-5 5v3h-3v4h3v8h4v-8h3l1-4h-4v-3a1 1 0 011-1h3v-4z" stroke="currentColor"
-                strokeLinecap="round" strokeLinejoin="round" />
-            </svg>
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/ebrains_eu" target="_blank" rel="noopener noreferrer" title="Twitter">
-            <svg className="sprite-icons" xmlns="http://www.w3.org/2000/svg" id="i-twitter" viewBox="0 0 40 40" fill="none">
-              <path
-                d="M31 11a10.897 10.897 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 0111 12s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5 0-.278-.028-.556-.08-.83A7.72 7.72 0 0031 11v0z"
-                stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
-            </svg>
-          </a>
-        </li>
-        {/* <li>
+          {/* <li>
             <a href="https://www.youtube.com/user/TheHumanBrainProject" target="_blank" rel="noopener" title="Youtube">
               <svg className="sprite-icons" xmlns="http://www.w3.org/2000/svg" id="i-youtube" viewBox="0 0 40 40" fill="none">
                 <path d="M30.54 14.42a2.78 2.78 0 00-1.94-2C26.88 12 20 12 20 12s-6.88 0-8.6.46a2.78 2.78 0 00-1.94 2A29.001 29.001 0 009 19.75a29 29 0 00.46 5.33A2.78 2.78 0 0011.4 27c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 001.94-2c.312-1.732.466-3.49.46-5.25a29.005 29.005 0 00-.46-5.33v0z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
@@ -134,36 +141,44 @@ const Footer = ({ commit, profile, theme }) => (
               </svg>
             </a>
           </li> */}
-        <li>
-          <a href="https://www.instagram.com/ebrains_eu/" target="_blank" rel="noopener noreferrer" title="Instagram">
-            <svg className="sprite-icons" xmlns="http://www.w3.org/2000/svg" id="i-instagram" viewBox="0 0 40 40" fill="none">
-              <path d="M25 10H15a5 5 0 00-5 5v10a5 5 0 005 5h10a5 5 0 005-5V15a5 5 0 00-5-5z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
-              <path d="M24 19.37a4 4 0 11-7.914 1.173A4 4 0 0124 19.37zM25.5 14.5h.01" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
-            </svg>
-          </a>
-        </li>
-        {/* <li>
+          <li>
+            <a href="https://www.instagram.com/ebrains_eu/" target="_blank" rel="noopener noreferrer" title="Instagram">
+              <svg className="sprite-icons" xmlns="http://www.w3.org/2000/svg" id="i-instagram" viewBox="0 0 40 40"
+                   fill="none">
+                <path d="M25 10H15a5 5 0 00-5 5v10a5 5 0 005 5h10a5 5 0 005-5V15a5 5 0 00-5-5z" stroke="currentColor"
+                      strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M24 19.37a4 4 0 11-7.914 1.173A4 4 0 0124 19.37zM25.5 14.5h.01" stroke="currentColor"
+                      strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
+            </a>
+          </li>
+          {/* <li>
           <a href="https://github.com/HumanBrainProject/" target="_blank" rel="noopener" title="GitHub">
             <svg className="sprite-icons" xmlns="http://www.w3.org/2000/svg" id="i-github" viewBox="0 0 40 40" fill="none">
               <path d="M24 30v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 00-1.5-3.75A5.07 5.07 0 0027.91 9s-1.18-.35-3.91 1.48a13.38 13.38 0 00-7 0C14.27 8.65 13.09 9 13.09 9a5.07 5.07 0 00-.09 3.77 5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 00-.94 2.58V30m0-3c-5 1.5-5-2.5-7-3l7 3z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
             </svg>
           </a>
         </li> */}
-        <li>
-          <a href="https://www.linkedin.com/company/ebrains-eu" target="_blank" rel="noopener noreferrer" title="Linkedin">
-            <svg className="sprite-icons" xmlns="http://www.w3.org/2000/svg" id="i-linkedin" viewBox="0 0 40 40" fill="none">
-              <path d="M24 16a6 6 0 016 6v7h-4v-7a2 2 0 00-4 0v7h-4v-7a6 6 0 016-6v0zM14 17h-4v12h4V17zM12 14a2 2 0 100-4 2 2 0 000 4z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
-            </svg>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </footer>
+          <li>
+            <a href="https://www.linkedin.com/company/ebrains-eu" target="_blank" rel="noopener noreferrer"
+               title="Linkedin">
+              <svg className="sprite-icons" xmlns="http://www.w3.org/2000/svg" id="i-linkedin" viewBox="0 0 40 40"
+                   fill="none">
+                <path
+                    d="M24 16a6 6 0 016 6v7h-4v-7a2 2 0 00-4 0v7h-4v-7a6 6 0 016-6v0zM14 17h-4v12h4V17zM12 14a2 2 0 100-4 2 2 0 000 4z"
+                    stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </div>
+      {profiles[profile]["copyrightAddition"] && <p className="footer__copyright_addition">{profiles[profile]["copyrightAddition"]}</p>}
+    </footer>
 );
 
 export default connect(
-  state => ({
-    commit: state.application.commit,
+    state => ({
+      commit: state.application.commit,
     profile: state.application.profile,
     theme: state.application.theme
   })
